063adec3a15cae34d1afc6a3ad9d919a5d6e5347
Ezequiel <eafocaraccio@gmail.com>
2020-11-23 23:54:31 -0300

    se cambia direccion de hosting

---
+++ docs/precache-manifest.ab9162af83ea844cfeafab3c81093931.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153235dfd9c9c4f0e3405eaf4883cf27",
+    "url": "/powereye-ui/index.html"
+  },
+  {
+    "revision": "b4e9f34b0bf86b88728b",
+    "url": "/powereye-ui/static/css/2.d0affde7.chunk.css"
+  },
+  {
+    "revision": "3c2901a505e0e4b6c56e",
+    "url": "/powereye-ui/static/css/main.3be1e55f.chunk.css"
+  },
+  {
+    "revision": "b4e9f34b0bf86b88728b",
+    "url": "/powereye-ui/static/js/2.db5332fc.chunk.js"
+  },
+  {
+    "revision": "38b5318ef2c47c18f9fad859a3d4ae27",
+    "url": "/powereye-ui/static/js/2.db5332fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2901a505e0e4b6c56e",
+    "url": "/powereye-ui/static/js/main.e230aefe.chunk.js"
+  },
+  {
+    "revision": "ea46fdf7150f90b0b91b",
+    "url": "/powereye-ui/static/js/runtime-main.9c6bdb4c.js"
+  },
+  {
+    "revision": "4651b459d46cf7073563c82334b32706",
+    "url": "/powereye-ui/static/media/PowerEyeLogoAndText.4651b459.png"
+  },
+  {
+    "revision": "5f46b29ef7aa66799b00acbdb3a3315b",
+    "url": "/powereye-ui/static/media/PowerEyeOnlyLogo.5f46b29e.png"
+  },
+  {
+    "revision": "dce1d84b668c27f3852f8463d34ec79b",
+    "url": "/powereye-ui/static/media/PowerEyeTextLogo.dce1d84b.png"
+  },
+  {
+    "revision": "dccfe1ee706aeb3d45f13b1424a2673a",
+    "url": "/powereye-ui/static/media/chart-color-bg.dccfe1ee.png"
+  },
+  {
+    "revision": "92441e2f4e29b7a4f2e69417c7547153",
+    "url": "/powereye-ui/static/media/config-color-bg.92441e2f.png"
+  },
+  {
+    "revision": "3d7c39c5fbc55c13dc4c215f793f6560",
+    "url": "/powereye-ui/static/media/glass.3d7c39c5.png"
+  }
+]);
