3b4173b681ff33bbbbb80cfd87ed3e3d0a1c55f3
Ezequiel <eafocaraccio@gmail.com>
2020-11-23 23:44:04 -0300

    se agrega docs folder para host

---
+++ docs/precache-manifest.483f45107ae64feb65adf3a0d340e844.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598f63e04e9c2b191b54e8942dcd258c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45b4e977c8a125756e38",
+    "url": "/static/css/2.d0affde7.chunk.css"
+  },
+  {
+    "revision": "31028593d60332d61753",
+    "url": "/static/css/main.642a4b34.chunk.css"
+  },
+  {
+    "revision": "45b4e977c8a125756e38",
+    "url": "/static/js/2.faf6f74b.chunk.js"
+  },
+  {
+    "revision": "38b5318ef2c47c18f9fad859a3d4ae27",
+    "url": "/static/js/2.faf6f74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31028593d60332d61753",
+    "url": "/static/js/main.e6ed7728.chunk.js"
+  },
+  {
+    "revision": "74ce9532618d87cd9079",
+    "url": "/static/js/runtime-main.b3f5b90d.js"
+  },
+  {
+    "revision": "4651b459d46cf7073563c82334b32706",
+    "url": "/static/media/PowerEyeLogoAndText.4651b459.png"
+  },
+  {
+    "revision": "5f46b29ef7aa66799b00acbdb3a3315b",
+    "url": "/static/media/PowerEyeOnlyLogo.5f46b29e.png"
+  },
+  {
+    "revision": "dce1d84b668c27f3852f8463d34ec79b",
+    "url": "/static/media/PowerEyeTextLogo.dce1d84b.png"
+  },
+  {
+    "revision": "dccfe1ee706aeb3d45f13b1424a2673a",
+    "url": "/static/media/chart-color-bg.dccfe1ee.png"
+  },
+  {
+    "revision": "92441e2f4e29b7a4f2e69417c7547153",
+    "url": "/static/media/config-color-bg.92441e2f.png"
+  },
+  {
+    "revision": "3d7c39c5fbc55c13dc4c215f793f6560",
+    "url": "/static/media/glass.3d7c39c5.png"
+  }
+]);
